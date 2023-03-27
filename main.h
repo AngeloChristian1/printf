e12a34e31c893f5cea9267c65bdca68c849a56dd
Angelo Christian <ishgatetechristian@gmail.com>
2023-03-27 06:39:39 -0700

    Adding to 12

--- main.h
+++ main.h
@@ -42,9 +42,6 @@ typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
-
-/****************** FUNCTIONS ******************/
-
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
@@ -115,3 +112,5 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
+
+
